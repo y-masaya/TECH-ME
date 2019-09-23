9ee9a70b109074e7a0707604bc201d0edc623aa7
y-masaya <my.927ac@gmail.com>
2019-09-23 16:57:09 +0900

    Create-Article-view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,14 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    unless @article.save
+      redirect_to new_article_path
+    end
+  end
 end
