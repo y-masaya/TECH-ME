2cb2d551f7504c9ec1a067612727641ab79045bd
y-masaya <my.927ac@gmail.com>
2019-09-26 14:46:45 +0900

    Create-question-view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,27 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.order(id: "DESC").page(params[:page]).per(5)
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:category_id, :title, :content).merge(user_id: current_user.id)
+  end
+
 end
